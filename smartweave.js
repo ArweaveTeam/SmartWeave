3d1f0cbca52702e861de25adaa4a7f0d78b280a1
Sam Williams <samuel.ecwilliams@gmail.com>
2020-04-30 20:29:59 +0200

    Replaced tip detection mechanism. Still needs testing.

--- smartweave.js
+++ smartweave.js
@@ -81,7 +81,12 @@ module.exports = {
         const contractSrcTXID = contractTX.get('tags')['Contract-Src']
         const contractSrcTX = await arweave.transactions.get(contractSrcTXID)
         const contractSrc = contractSrcTX.get('data', {decode: true, string: true})
-        const state = JSON.parse(tipTX.get('data', {decode: true, string: true}))['newState']
+
+        if(tipTX.get('tags')['Type'] == "Contract")
+            const state = tipTX.get('data', {decode: true, string: true})
+        else
+            const state = JSON.parse(tipTX.get('data', {decode: true, string: true}))['newState']
+        
         const address = await arweave.wallets.jwkToAddress(wallet)
 
         // Calcualte the state after our new TX has been processed.
@@ -89,11 +94,6 @@ module.exports = {
 
         if(!newState)
             return false
-
-        // Generate POW, using last tip TXID as challenge.
-        // This creates a cost to contract submission (controllable by the contract
-        // initiator), discouraging spam (on top of the Arweave TX submission fee).
-        const nonce = this.generatePOW(tipTX.id)
 
         // Package new state into new TX, add POW
         let interactionTX = arweave.createTransaction({
@@ -104,11 +104,9 @@ module.exports = {
         interactionTX.addTag('Type', 'interaction')
         interactionTX.addTag('With-Contract', contractID)
         interactionTX.addTag('Previous-TX', tipTX.id)
-        interactionTX.addTag('Nonce', nonce)
         interactionTX.addTag('Version', '0.0.1')
 
         const response = await arweave.transactions.post(interactionTX)
-        console.log(response.status)
 
         if(response.status != 200)
             return false
@@ -118,10 +116,19 @@ module.exports = {
 
     findContractTip: async function(arweave, contractID) {
         const contract = await this.getContract(arweave, contractID)
+        let current = contract.contractTX
 
-        // Find all of the contract interaction transactions, ordered by newest 
-        // first. Currently, block order will determine which transactions are valid.
-        let tipQuery =
+        do {
+            last = current
+            current = this.findNextTX(arweave, contract, current)
+        }
+        while(current)
+
+        return last
+    },
+
+    findNextTX: async function(arweave, contract, state, currentTX) {
+        let successorsQuery =
             {
                 op: 'and',
                 expr1:
@@ -133,57 +140,35 @@ module.exports = {
                 expr2:
                     {
                         op: 'equals',
-                        expr1: 'With-Contract',
-                        expr2: contractID
+                        expr1: 'Previous-TX',
+                        expr2: currentTX.id
                     }
             }
-        const possibleTips = await this.arweave.api.post(`arql`, tipQuery)
+        const results = await this.arweave.api.post(`arql`, successorsQuery)
         
-        // If there are no TXs on the contract yet, return the contract ID.
-        if(possibleTips.data == '')
-            return contractID
-        
-        // Else, Attempt to validate each tip in turn, until a valid path is found.
-        for(i = 0; i < possibleTips.length; i++) {
-            let tipTX = validateContractTip(arweave, contract, possibleTips[i])
-            if(!tipTX)
-                return tipTX
+        let successors = (result == '') ? [] : results
+
+        for(let i = 0; i < successors.length; i++) {
+            let TX = await arweave.transactions.get(successors[i])
+            if(this.validateNextTX(contract, state, TX))
+                return TX
         }
+
+        return false
     },
 
-    validateContractTip: async function(arweave, contract, contractSrc, tipTXID) {
-        // Validate POWs, working from the tip towards the contract TX.
-        // Along the way, build up a stack of transactions (and contained state
-        // transitions) to validate.
-        let transitionsToValidate = []
-        let tipTX = await arweave.transactions.get(tipTXID)
-        let currentTX = tipTX
-        let previousTXID = tipTX.get('tags')['Previous-TX']
+    validateNextTX: async function(contract, state, nextTX) {
+        if(nextTX.get('tags')['Type'] == "Contract")
+            const newState = nextTX.get('data', {decode: true, string: true})
+        else
+            const newState = JSON.parse(nextTX.get('data', {decode: true, string: true}))['newState']
 
-        while(previousTXID != contract.id) {
-            if(!this.verifyPOW(previousTXID, currentTX.get('tags')['Nonce'], contract.minDiff))
-                return false
-            transitionsToValidate.push(currentTX)
-            if(!(currentTX = await arweave.transactions.get(previousTXID)))
-                return false
-            if(!(previousTXID = currentTX.get('tags')['Previous-TX']))
-                return false
-        }
-
-        // Validate state transitions that have been pushed to the stack.
-
-        let state = contract.initState
-        while(transitionsToValidate.length > 0) {
-            let nextTX = transitionsToValidate.pop()
-            let caller = await arweave.wallets.ownerToAddress(nextTX.owner)
-            let struct = JSON.parse(nextTX.get('data', {decode: true, string: true}))
-            if(!this.validateStateTransition(contract.contractSrc, state, struct.input, struct.newState, caller))
-                return false
-            state = struct.newState
-        }
-
-        // Path is valid! Return the tip TX.
-        return tipTX
+        return this.validateStateTransition(
+                    contract.contractSrc,
+                    state,
+                    struct.input,
+                    newState,
+                    await arweave.wallets.ownerToAddress(nextTX.owner))
     },
 
     getContract: async function(arweave, contractID) {
@@ -199,15 +184,8 @@ module.exports = {
             id: contractID,
             contractSrc: contractSrc,
             initState: state,
-            minDiff: minDiff
+            minDiff: minDiff,
+            contractTX: contractTX
         }
-    },
-
-    generatePOW: async function(challenge) {
-        return ""
-    },
-
-    verifyPOW: async function(challenge, nonce, minDiff) {
-        return true
     }
  }
