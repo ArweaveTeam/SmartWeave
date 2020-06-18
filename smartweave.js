4d416b30a3329ff523b0e94345526228fbc5fb93
aidanok <52333119+aidanok@users.noreply.github.com>
2020-06-18 09:55:55 +0100

    Put input in tags and remove reliance on Previous-TX
    This reworks things so that the interaction txs do not
    rely on knowing the exiting state, and to get to the contracts
    current state you just replay { initstate } + [tx, tx, tx] + { code }
    
    Tx's are ordered by { block_height, sha256(indep_hash + txid) }
    Unconfirmed transactions can not be included, as we don't have
    indep_hash for them.

--- smartweave.js
+++ smartweave.js
@@ -1,3 +1,5 @@
+const { retryWithBackoff, batch, softFailWith } = require('promises-tho');
+
 module.exports = {
     createContract: async function(arweave, wallet, contractSrc, initState, minDiff) {
         // Create a TX to store the contract source, then create a contract from that src.
@@ -6,7 +8,7 @@ module.exports = {
         let srcTX = await arweave.createTransaction({ data: contractSrc }, wallet)
         srcTX.addTag('App-Name', 'SmartWeave')
         srcTX.addTag('Type', 'contractSrc')
-        srcTX.addTag('Version', '0.0.1')
+        srcTX.addTag('Version', '0.2.0')
         await arweave.transactions.sign(srcTX, wallet)
 
         const response = await arweave.transactions.post(srcTX)
@@ -23,7 +25,7 @@ module.exports = {
         contractTX.addTag('App-Name', 'SmartWeave')
         contractTX.addTag('Type', 'contract')
         contractTX.addTag('Contract-Src', srcTXID)
-        contractTX.addTag('Version', '0.0.1')
+        contractTX.addTag('Version', '0.2.0')
 
         await arweave.transactions.sign(contractTX, wallet)
 
@@ -34,6 +36,7 @@ module.exports = {
             return false
     },
 
+    // un-used
     getState: async function(arweave, contractID) {
         // Return the current state (as a string) for a contract.
         const tipTX = await this.findContractTip(arweave, contractID)
@@ -41,6 +44,85 @@ module.exports = {
             return false
         
         return this.getTXState(tipTX)
+    },
+
+    // Replays all interactions with the contract to derive the current state
+    // height optionally limits the heigh we replay to. 
+    // Returns state as plain Js object.
+    replayToState: async function(arweave, contractID, height = Number.POSITIVE_INFINITY) {
+        
+        const contractInfo = await this.getContract(arweave, contractID);
+
+        let state; 
+        try {
+            state = JSON.parse(contractInfo.initState);
+        } catch (e) {
+            throw new Error(`Unable to parse initial state for contract: ${contractID}`);
+        }
+        
+        // Load all the interaction txs relevant to this contract. 
+
+        // This can be made a lot cleaner with some GraphQL features, 
+        // (block info in results, pagination)
+        // but for now, we stick with arql and use some utils to help 
+        // with concurency and retry on errors. (we can be firing off thousands of requests here) 
+        
+        const arql = {
+            op: 'and',
+            expr1: {
+                op: 'equals',
+                expr1: 'App-Name',
+                expr2: 'SmartWeave',
+            },
+            expr2: {
+                op: 'equals',
+                expr1: 'With-Contract',
+                expr2: contractID
+            }
+        }
+        
+        let transactions = await arweave.arql(arql);
+        console.log(`Query returned ${transactions.length} interactions`);
+
+        const getTxInfoFn = retryWithBackoff(
+            { tries: 2, startMs: 1000 }, 
+            (id) => this.getFullTxInfo(arweave, id)
+        );
+        const batcher = batch(
+            { batchDelayMs: 50, batchSize: 6 },
+            softFailWith(null, getTxInfoFn)
+        )
+        let txInfos = await batcher(transactions);
+
+        // Filter out txs that are not confirmed yet, not found, or are below the 
+        // height we are replaying to.
+        txInfos = txInfos.filter(x => !!x && x.info.confirmed.block_height <= height);
+        // Sort
+        console.log(`Replaying ${txInfos.length} to get current state`);
+        txInfos.sort((a, b) => a.sortKey.localeCompare(b.sortKey));
+
+        for (let i = 0; i < txInfos.length; i++) {
+            let input;
+            
+            try { 
+                input = JSON.parse(this.getTag(txInfos[i].tx, 'Input'))
+            } catch (e) {}
+
+            if (!input) {
+                console.warn(`Skipping tx with missing or invalid Input tag - ${txInfos[i].id}`);
+                continue;
+            }
+            
+            let nextState = await this.execute(contractInfo.contractSrc, input, state, txInfos[i].from);
+            if (!nextState) {
+                console.warn(`Executing of interaction: ${txInfos[i].id} threw exception, skipping`);
+                continue;
+            }
+
+            state = nextState;
+        }
+
+        return state; 
     },
 
     execute: async function(contractSrc, input, state, caller) {
@@ -58,7 +140,7 @@ module.exports = {
         try {
             eval(contractSrc)
         } catch (err) {
-            //console.error(err);
+            console.error(err);
             return false
         }
 
@@ -66,10 +148,53 @@ module.exports = {
         return state
     },
 
+    // Interaction that writes a state change. 
+    interactWrite: async function(arweave, wallet, contractID, input) {
+
+        // Workaround we set data to something 
+        // otherwise tx will not be valid, since it has no data 
+        // and no target. 
+        let interactionTX = await arweave.createTransaction({
+            data: Math.random().toString()
+        }, wallet)
+
+        interactionTX.addTag('App-Name', 'SmartWeave')
+        interactionTX.addTag('Type', 'interaction')
+        interactionTX.addTag('With-Contract', contractID)
+        interactionTX.addTag('Version', '0.2.0')
+        interactionTX.addTag('Input', JSON.stringify(input))
+
+        await arweave.transactions.sign(interactionTX, wallet)
+
+        const response = await arweave.transactions.post(interactionTX)
+
+        if(response.status != 200)
+            return false
+        
+        return interactionTX.id
+    },
+
+    interactWriteDryRun: async function(arweave, wallet, contractID, input) {
+        const contractInfo = await this.getContract(arweave, contractID);
+        const latestState = await this.replayToState(arweave, contractID);
+        const result = await this.execute(contractInfo.contractSrc, input, latestState, await arweave.wallets.jwkToAddress(wallet)); 
+        if (!result) {
+            return {
+                success: false 
+            }
+        }
+        return {
+            success: true,
+            state: result
+        }
+    },
+
+    // un-used
     validateStateTransition: async function(contractSrc, state, input, newState, caller) {
         return this.execute(contractSrc, input, state, caller) == newState
     },
 
+    // un-used
     interact: async function(arweave, wallet, contractID, input, dryRun) {
         // Call a contract with new input, storing the resulting TX on-weave.
         // In order to execute this, the client first locates the top valid tip in the network,
@@ -104,7 +229,7 @@ module.exports = {
         interactionTX.addTag('Type', 'interaction')
         interactionTX.addTag('With-Contract', contractID)
         interactionTX.addTag('Previous-TX', tipTX.id)
-        interactionTX.addTag('Version', '0.0.1')
+        interactionTX.addTag('Version', '0.2.0')
 
         await arweave.transactions.sign(interactionTX, wallet)
 
@@ -116,6 +241,7 @@ module.exports = {
         return interactionTX.id
     },
 
+    // un-used
     findContractTip: async function(arweave, contractID) {
         const contract = await this.getContract(arweave, contractID)
         let current = contract.contractTX
@@ -131,6 +257,7 @@ module.exports = {
         return last
     },
 
+    // un-used 
     findNextTX: async function(arweave, contract, state, currentTX) {
         let successorsQuery =
             {
@@ -160,6 +287,47 @@ module.exports = {
         }
 
         return false
+    },
+
+    // This gets the full Tx Info, and calcutes a sort key.
+    // It needs to get the block_height and indep_hash from
+    // the status endpoint as well as the tx itself. If the 
+    // tx is confirmed it assigns it a sort key, otherwise it
+    // does not. 
+    getFullTxInfo: async function(arweave,id) {
+        const [tx, info] = await Promise.all([
+            arweave.transactions.get(id).catch(e => {
+                if (e.type === 'TX_PENDING') {
+                    return undefined
+                } 
+                throw(e);
+            }),
+            arweave.transactions.getStatus(id)
+        ])
+
+        if (!tx || !info.confirmed) {
+            return undefined;
+        }
+        
+        function arrayToHex(arr) {
+            let str = ''
+            for (let i = 0; i < arr.length; i++) {
+                str += ("0"+arr[i].toString(16)).slice(-2)
+            }
+            return str;
+        }
+
+        // Construct a string that will lexographically sort.
+        // { block_height, sha256(block_indep_hash + txid) }
+        const blockHashBytes = arweave.utils.b64UrlToBuffer(info.confirmed.block_indep_hash)
+        const txIdBytes = arweave.utils.b64UrlToBuffer(id)
+        const concatted = arweave.utils.concatBuffers([blockHashBytes, txIdBytes])
+        const hashed = arrayToHex(await arweave.crypto.hash(concatted))
+        const block_height = `000000${info.confirmed.block_height}`.slice(-12); 
+    
+        const sortKey = `${block_height},${hashed}`
+        
+        return { tx, info, id: tx.id, sortKey, from: await arweave.wallets.ownerToAddress(tx.owner) }
     },
 
     validateNextTX: async function(arweave, contract, state, nextTX) {
