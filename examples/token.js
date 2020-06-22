e1cdf6dd611093f9b7a3729ed4d400e89db17d63
aidanok <52333119+aidanok@users.noreply.github.com>
2020-06-22 11:24:00 +0100

    make token.js handle non async + formatting

--- examples/token.js
+++ examples/token.js
@@ -1,53 +1,50 @@
 
-export async function handle(state, action) {
+export function handle(state, action) {
+  
+  let balances = state.balances;
+  let input = action.input;
+  let caller = action.caller;
 
-    let balances = state.balances;
-    let input = action.input;
-    let caller = action.caller;
-    
-    if (input.function == 'transfer') {
-      
-      let target = input.target;
-      let qty = Math.trunc(input.quantity);
-  
-      if (!target) {
-        throw new ContractError(`No target specified`);
-      }
-  
-      if (isNaN(qty)) {
-        throw new ContractError(`Invalid quantity`);
-      }
-  
-      if (qty <= 0 || caller == target) {
-        throw new ContractError('Invalid token transfer');
-      }
-  
-      if (balances[caller] < qty) {
-        throw new ContractError(`Caller balance not high enough to send ${qty} token(s)!`);
-      }
-  
-      // Lower the token balance of the caller
-      balances[caller] -= qty;
-      if (target in balances) {
-        // Wallet already exists in state, add new tokens
-        balances[target] += qty;
-      } else {
-        // Wallet is new, set starting balance
-        balances[target] = qty;
-      }
-      
-      return { state }
+  if (input.function == 'transfer') {
+    let target = input.target;
+    let qty = Math.trunc(input.quantity);
+
+    if (!target) {
+      throw new ContractError(`No target specified`);
     }
-    
-    if (input.function == 'balance') {
-      let target = input.target;
-      let ticker = state.ticker;
-      let divisibility = state.divisibility;
-      let balance = balances[target] / divisibility;
-      return { result: { target, ticker, balance } };
+
+    if (isNaN(qty)) {
+      throw new ContractError(`Invalid quantity`);
     }
-  
-    throw new ContractError(`No function supplied or function not recognised: "${input.function}"`);
-    
+
+    if (qty <= 0 || caller == target) {
+      throw new ContractError('Invalid token transfer');
+    }
+
+    if (balances[caller] < qty) {
+      throw new ContractError(`Caller balance not high enough to send ${qty} token(s)!`);
+    }
+
+    // Lower the token balance of the caller
+    balances[caller] -= qty;
+    if (target in balances) {
+      // Wallet already exists in state, add new tokens
+      balances[target] += qty;
+    } else {
+      // Wallet is new, set starting balance
+      balances[target] = qty;
+    }
+
+    return { state };
   }
-  
+
+  if (input.function == 'balance') {
+    let target = input.target;
+    let ticker = state.ticker;
+    let divisibility = state.divisibility;
+    let balance = balances[target] / divisibility;
+    return { result: { target, ticker, balance } };
+  }
+
+  throw new ContractError(`No function supplied or function not recognised: "${input.function}"`);
+}
