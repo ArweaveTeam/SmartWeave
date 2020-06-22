35fbf8a493020a8f36c5c57f1ac6621adec1911b
aidanok <52333119+aidanok@users.noreply.github.com>
2020-06-22 11:05:25 +0100

    update token.js to new interface

--- examples/token.js
+++ examples/token.js
@@ -1,41 +1,53 @@
-let balances = state.balances
 
-if(input.function == "transfer") {
+export async function handle(state, action) {
+
+    let balances = state.balances;
+    let input = action.input;
+    let caller = action.caller;
     
-    let target = input.target
-    let qty = Math.trunc(input.quantity)
-
-    if((qty <= 0) || (caller == target)) {
-        throw "Invalid token transfer."
+    if (input.function == 'transfer') {
+      
+      let target = input.target;
+      let qty = Math.trunc(input.quantity);
+  
+      if (!target) {
+        throw new ContractError(`No target specified`);
+      }
+  
+      if (isNaN(qty)) {
+        throw new ContractError(`Invalid quantity`);
+      }
+  
+      if (qty <= 0 || caller == target) {
+        throw new ContractError('Invalid token transfer');
+      }
+  
+      if (balances[caller] < qty) {
+        throw new ContractError(`Caller balance not high enough to send ${qty} token(s)!`);
+      }
+  
+      // Lower the token balance of the caller
+      balances[caller] -= qty;
+      if (target in balances) {
+        // Wallet already exists in state, add new tokens
+        balances[target] += qty;
+      } else {
+        // Wallet is new, set starting balance
+        balances[target] = qty;
+      }
+      
+      return { state }
     }
-
-    // Don't do anything unless we have enough tokens
-    if(balances[caller] >= qty) {
-        // Lower the token balance of the caller
-        balances[caller] -= qty
-        if(target in balances) {
-            // Wallet already exists in state, add new tokens
-            balances[target] += qty
-        }
-        else {
-            // Wallet is new, set starting balance
-            balances[target] = qty
-        }
-        state.balances = balances
+    
+    if (input.function == 'balance') {
+      let target = input.target;
+      let ticker = state.ticker;
+      let divisibility = state.divisibility;
+      let balance = balances[target] / divisibility;
+      return { result: { target, ticker, balance } };
     }
-    else {
-        throw "Caller balance not high enough to send " + qty + " token(s)!"
-    }
-}
-else if(input.function == "balance") {
-    let target = input.target
-    let ticker = state.ticker
-    let divisibility = state.divisibility
-    let balance = balances[target] / divisibility
-    console.log(
-        "The balance of wallet " + target +
-        " is " + balance + " " + ticker + ".")
-}
-else {
-    throw "Function not recognised."
-}
+  
+    throw new ContractError(`No function supplied or function not recognised: "${input.function}"`);
+    
+  }
+  
