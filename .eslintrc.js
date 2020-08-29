7c1cacc98f85033901e0c0e09669363b9365a756
Sam <hatem@clearrain.xyz>
2020-08-28 20:11:16 -0400

    chore: added linting setup

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json'
+  },
+  root: true,
+  env: {
+    node: true,
+    es6: true
+  },
+  extends: 'standard-with-typescript',
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
+  },
+  rules: {
+    '@typescript-eslint/promise-function-async': 0,
+    '@typescript-eslint/restrict-template-expressions': 0,
+    '@typescript-eslint/strict-boolean-expressions': 0,
+    '@typescript-eslint/explicit-function-return-type': 0,
+    '@typescript-eslint/prefer-optional-chain': 0,
+    'no-prototype-builtins': 0,
+    '@typescript-eslint/no-var-requires': 0,
+    '@typescript-eslint/restrict-plus-operands': 0,
+    '@typescript-eslint/prefer-nullish-coalescing': 0
+  }
+}
