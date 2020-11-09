9e03db98154a3da9d6d51e124e5b35936568fda3
Cedrik <cedrik.git@tryninja.io>
2020-11-09 09:52:46 -0600

    browser build version

---
+++ build.js
@@ -0,0 +1,38 @@
+const { build } = require('esbuild');
+const rimraf = require('rimraf');
+
+const clean = async () => {
+  return new Promise(resolve => {
+    rimraf('./dist', () => resolve());
+  });
+}
+
+const runBuild = async (doClean = false) => {
+  // Do not clean each time on watch, only on build or first run.
+  if(doClean) await clean();
+ 
+  // Build to browser js
+  build({
+    entryPoints: ['./src/index.ts'],
+    minify: false,
+    bundle: true,
+    outfile: './dist/smartweave.js'
+  }).catch((e) => {
+    console.log(e);
+    process.exit(1)
+  });
+
+  // Minified version
+  build({
+    entryPoints: ['./src/index.ts'],
+    minify: true,
+    bundle: true,
+    outfile: './dist/smartweave.min.js'
+  }).catch((e) => {
+    console.log(e);
+    process.exit(1)
+  });
+};
+runBuild(true);
+
+module.exports = runBuild;
